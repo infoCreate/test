b21249ee7b44ed8222bab8444b2683f019d633a8
Alexandr Batiuk <batiuk@MacBook-Air-Alexandr.local>
2026-05-26 17:38:58 +0300

    lesson04

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-// const Firstname = "Jonson";
-// console.log("Hello", Firstname);
-let Firstname = "Jonson";
-Firstname = "Doe";
-console.log("Hello", Firstname);
+// let x = 11;
+
+// if (x > 10) {
+//   console.log("условие верно");
+// } else {
+//   console.log("условие не верно");
+// }
+
+// let a = 5;
+// let b = "10";
+// let res = a + +(+b);
+// console.log(a.toString());
+
+// console.log(res);
+
+// console.log(a + b);
+// console.log(typeof a);
+// console.log(typeof b);
+
+// alert("Привет, я JavaScript!");
+
+let age = prompt("Сколько вам лет?");
+console.log(age);
+let a = 10;
+let result = a + +age;
+console.log(result);
