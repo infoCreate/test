43745e58a96686905b5f4bdd5a259120cc359f9c
Alexandr Batiuk <batiuk@MacBook-Air-Alexandr.local>
2026-05-26 14:22:39 +0300

    lesson03

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-//const Firstname = "Jonson";
-//console.log("Hello", Firstname);
+// const Firstname = "Jonson";
+// console.log("Hello", Firstname);
 let Firstname = "Jonson";
 Firstname = "Doe";
 console.log("Hello", Firstname);
---
+++ lesson03/lesson-03.js
@@ -0,0 +1,15 @@
+//titleProject (string)
+//screensValue (string)
+//screenPrice (num)
+//percentage (num)
+//fullPrice (num)
+//responsive (bool)
+
+let titleProject = "Калькулятор по разработке сайта";
+let screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+let screenPrice = 10000;
+let percentage = 10;
+let fullPrice = 100000;
+let responsive = true;
+
+console.log(titleProject);
